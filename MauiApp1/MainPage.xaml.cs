b5e94b491b349ee58b92beb3eff98d90acaa41f6
Nathan Jeynes <nathan@medialogix.co.uk>
2024-01-08 18:33:29 +0000

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void ContentPage_Loaded(object sender, EventArgs e)
+        {
+#if IOS || ANDROID
+
+            //try
+            //{
+            //    CommunityToolkit.Maui.Core.Platform.StatusBar.SetColor(Color.FromArgb("#282828"));
+            //}
+            //catch (Exception)
+            //{
+
+
+            //}
+
+
+#endif
+        }
+    }
+}
